a6c61bf510a4246164e4eda98e9d4856f701560f
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-27 15:57:38 +0700

    Completed reace components project.

--- 3-react-components/src/components/App.js
+++ 3-react-components/src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
+import Message from './Message';
+import Segment from './Segment';
 import faker from 'faker';
 import moment from 'moment';
 
@@ -47,6 +49,33 @@ const App = () => {
             </ApprovalCard>
 
 
+            <div>
+                <Message 
+                    header="Changes in Service"
+                    text="We just updated our privacy policy here to better service our customers."
+                />
+            </div>
+
+            <div><br /></div>
+
+            <div>
+                <Segment>
+                    <div className="ui icon header">
+                        <i className="pdf file outline icon"></i>
+                        No documents are listed for this customer.
+                    </div>
+                    <div className="ui primary button">Add Document</div>
+                </Segment>
+                <Segment>
+                    <h4 class="ui header">For Your Information</h4>
+                    <p>
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+                    </p>
+                </Segment>
+            </div>
+
+            <div><br /></div>
+            
         </div>
     )
 };
---
+++ 3-react-components/src/components/Segment.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Segment = props => {
+    return <div className="ui placeholder segment">{props.children}</div>
+};
+
+export default Segment;
