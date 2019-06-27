b3571eb29dce76d03cea12d18d1e00f6d4809aec
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-06-27 16:58:52 +0700

    Finished S4 Structuring Apps with Class-Based.

---
+++ 4-seasons/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+
+    render() {    
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );    
+
+        return <div>Latitude: </div>;
+    }
+}
+
+export default App;
---
+++ 4-seasons/src/components/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SeasonDisplay extends React.Component {
+    render() {
+        return <div>SeasonDisplay</div>
+    }
+}
+
+export default SeasonDisplay;
