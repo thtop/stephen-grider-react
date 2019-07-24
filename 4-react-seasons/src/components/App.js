1a6e3d9de957a6d19536426a3a890b62a53c662f
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-24 22:24:17 +0700

    Finished item 58. Introducting Lifecycle Methods

--- 4-react-seasons/src/components/App.js
+++ 4-react-seasons/src/components/App.js
@@ -16,6 +16,14 @@ class App extends React.Component {
     );
   }
 
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
+  }
+
+  componentDidUpdate() {
+    console.log('My component was just uptate - it rerendered!');
+  }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
