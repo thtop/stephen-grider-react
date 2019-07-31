3b0999dd198cf202f00935c2ae7cc4902a1ded1b
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-07-31 09:21:13 +0700

    Finished item 149. Building a List with Redux Data

--- 8-react-songs/src/components/App.js
+++ 8-react-songs/src/components/App.js
@@ -4,8 +4,12 @@ import SongList from "./SongList";
 
 const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className="ui container grid" style={{marginTop: '30px'}}>
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 };
